de0b59b7f275e5e5117321a414e06e9c4c3637f3
Patsoks <patsoks.sokari@gmail.com>
2023-03-17 08:27:33 -0700

    2. Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *	main - program that prints the alphabet in lower case
+ *	Return: returns 0
+ */
+int main(void)
+{
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

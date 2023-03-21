2327c2d9fb30523a58eb577ff45ecdf3993a82aa
Patsoks <patsoks.sokari@gmail.com>
2023-03-21 13:49:47 -0700

    4. Is Alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _isalpha - determines if an input char is lowercase, uppercase
+ * @c: the character under test
+ * Return: 1 if lowercase. 0 elsewhere
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') ? (c >= 'A' && c <= 'Z') ? 1 : 0 : 0);
+}

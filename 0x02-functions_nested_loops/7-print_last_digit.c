d28e9567d6b3e64b674f2e17e8c7d28daba1d15e
Patsoks <patsoks.sokari@gmail.com>
2023-03-21 14:54:43 -0700

    7. Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - determines the abs value of input int
+ * @n: the integer under test
+ * Return: 0 if lowercase
+ */
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+}

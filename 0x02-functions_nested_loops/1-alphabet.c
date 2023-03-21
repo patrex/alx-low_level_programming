8a2221ae51f70cfb1c948cc3da96a86f5421a0fc
Patsoks <patsoks.sokari@gmail.com>
2023-03-21 09:49:26 -0700

    1. Attempt a central design

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * main - writes the English alphabet to stdout
+ * Return: On success 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_aphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - writes the character c to stdout
+ *
+ * Return: On success 0.
+ */
+void print_alphabet(void)
+{
+	int c = 'a';
+        while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
+

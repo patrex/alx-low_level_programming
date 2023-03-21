c247ff9b08db7b8f9ff95cccc81ceb502630568f
Patsoks <patsoks.sokari@gmail.com>
2023-03-21 07:06:03 -0700

    0. Putchar: fix(1): correct linting issues

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+/**
+ * main.h - contains prototypes of all functions
+ * @c: - the character to print
+ *
+ * Return: 1 on success
+ * Return -1 on failure; set errno appropriately
+ */
+
+int _putchar(char c);

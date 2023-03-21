9aec51d2b8565ee3d64ceaae8f9fba14126766e0
Patsoks <patsoks.sokari@gmail.com>
2023-03-21 16:01:41 -0700

    10. Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - determines if an input char is lowercase, uppercase
+ * @m: the number under test
+ * @n: second number
+ * Return: 1 if positive. 0 if zero, -1 if negative
+ */
+int add(int m, int n)
+{
+	return (m + n);
+}

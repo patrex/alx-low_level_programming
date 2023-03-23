19b3227c32c285c298c72fedd549be43596817cd
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-24 00:35:37 +0100

    2. Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - returns the product of two ints
+ * @a: the first int
+ * @b: the second int
+ * Return: the product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}

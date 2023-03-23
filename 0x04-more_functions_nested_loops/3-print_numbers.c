6abbd6b73c94ebb815a2ec89e90c1b445271af57
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-24 00:47:14 +0100

    3. Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints the chars 0 - 9
+ * Return: no value
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
+	_putchar('\n');
+}

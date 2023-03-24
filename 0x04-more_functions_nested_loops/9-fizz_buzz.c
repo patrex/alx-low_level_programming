cb0b579d42e44dcaeaf6173b96d67423fd855d41
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-24 04:04:30 +0100

    9. FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints fizz buzz
+ * Return:  0
+ */
+void main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 5 == 0 && number % 3 == 0)
+			printf("FizzBuzz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else if (number % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", number);
+	}
+}

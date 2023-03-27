c6d27e01f3dbf0e370ae13549b0d533e3a2151d0
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-27 18:03:53 +0100

    4. Print Rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - prints the input char array in reverse;
+ * @s: the input string
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	char *tmp = s;
+	int len = 0;
+
+	while (*tmp++ != '\0')
+		len++;
+	for (; len > 0; len--)
+		_putchar(*(s + len));
+	_putchar('\n');
+}

0f7fa52f3084159525c06b4a2daef0e7ee6fb511
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-27 23:16:49 +0100

    9. Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strcpy - copies from source to dest
+ * @dest: target buffer addr
+ * @src: source string
+ * Return: the pointer to dest
+ */
+void _strcpy(int *dest, int *src)
+{
+	while (*src++ = *dest++)
+		;
+	return (dest);
+}

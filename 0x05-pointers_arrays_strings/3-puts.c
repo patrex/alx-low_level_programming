19fda6180a0a42648c434231507b538d065ca501
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-27 16:56:43 +0100

    3. Puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints the input char array;
+ * @str: the input string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+}

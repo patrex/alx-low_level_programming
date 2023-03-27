970423e022a6bb10fd21f44f23cd32910bede357
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-27 18:40:33 +0100

    5. Rev String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rev_string - reverses the input char array;
+ * @s: the input string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int k = 0;
+	char tmp;
+
+	while (s[len] != '\0')
+		len++;
+	len -= 1;
+	if (s[len] == '\n')
+		len -= 1;
+	while (k < len)
+	{
+		tmp = *s[k];
+		*s[k] = *s[len];
+		*s[len] = tmp;
+		len -= 1;
+		k += 1;
+	}
+}

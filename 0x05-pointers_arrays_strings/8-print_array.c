4e20b4398e727a533dc9e76338150c33f588843a
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-27 21:45:38 +0100

    8. Print Array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - prints the input int array;
+ * @a: the input int array
+ * @n: the number of elements to print
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+
+	if (n <= 0)
+		return;
+	
+	int i = 0;
+	
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i);
+		if ( i == (n - 1) continue;
+		_putchar(',');
+		_putchar(' ');
+	}
+}

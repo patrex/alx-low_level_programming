c79e545e6b28ece7e531e412ca3e26e6a48b5a42
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-30 23:22:29 +0100

    1. Strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings into one
+ * @dest: - the destination string
+ * @src: - the source string
+ * Return: the pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int count1, count2;
+
+	count1 = 0;
+	while (*(dest + count1))
+	{
+		if (*(dest + count1) != '\0')
+		{
+			count1++;
+		}
+	}
+
+	count2 = 0;
+	while (*(src + count2))
+	{
+		if (count2 < n)
+		{
+			*(dest + count1) = *(src + count2);
+		}
+		count1++;
+		count2++;
+		if (count2 == n)
+		{
+			break;
+		}
+	}
+	return (dest);
+}
+}

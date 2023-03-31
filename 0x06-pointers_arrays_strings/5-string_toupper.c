b591536347098033ac8055b90205e16841b16d7b
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-03-31 01:35:56 +0100

    5. String toupper: fix var names

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,10 @@ char *string_toupper(char *c)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (c[i] != '\0')
 	{
-		if ((str[i] >= 97) && (str[i] <= 122))
-			*(str + i) -= 32;
+		if ((c[i] >= 97) && (c[i] <= 122))
+			*(c + i) -= 32;
 		i++;
 	}
 	return (c);

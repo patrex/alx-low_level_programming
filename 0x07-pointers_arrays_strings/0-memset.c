5c12e00e0452eca5e72c439db9572ddf86e013c6
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-03 19:46:07 +0100

    0. Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - fills the area s by b
+ * @s: - destination string
+ * @b: - the constant char
+ * @n: - the number of bytes to copy
+ * Return: Pointer to s
+ */
+char *_memcpy(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0;  i < n && (*(dest + i)) = b; i++)
+	{
+	}
+
+	return (s);
+}

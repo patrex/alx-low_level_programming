ddafb5ef7829e15ce48a2849a87e1fd1644c878b
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-04 00:19:50 +0100

    3. Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - returns substring with char c
+ * @s: - the substring
+ * @accept: - the original string 
+ * Return: the len
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	int found = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		found = 0;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found = 0)
+		{
+			break;
+		}
+	}
+	return (i);
+}

bdc34057a723be56c342c4d89c8630722e57a07c
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-03 19:26:33 +0100

    1-Memcpy: refactor code: use for loop

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i++ < n && (*(dest + i) = *(src + i)))
+	for (i = 0;  i < n && (*(dest + i) = *(src + i)); i++)
 	{
 	}
+
 	return (dest);
 }

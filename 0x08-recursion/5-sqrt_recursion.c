aa51a487e4d266e72d576f2648eb284b69883ce5
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-06 03:01:48 +0100

    5. Square roots

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * power_operation - helper function: returns the natural square root of a number.
+ * @n: the number
+ * @c: iterator
+ * Return: the square root if foud else or -1.
+ */
+int check(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+		if (c * (n / c) == n)
+			return (c);
+		else
+			return (-1);
+	}
+	return (0 + power_operation(n, c + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (check(n, 2));
+}

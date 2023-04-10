135821f1803d310d1caa59eff89d10160ae31425
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-10 21:11:11 +0100

    0. Whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints its name to the console
+ * @argc: the number of arguments passed to the program
+ * @argv: the string array that holds the list of command-line args
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}

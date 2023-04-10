9016fbbf3c488fe21a6422643774d6e238486ca0
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-10 21:28:51 +0100

    1. Args count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints the number of args to std
+ * @argc: the number of arguments passed to the program
+ * @argv: the string array that holds the list of command-line args
+ * Return: returns 0
+ */
+int main(int __attribute__ ((unused)) argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}

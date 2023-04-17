1e53e70e2b3d76c64f8f67e17e84fedd9bd45d70
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-17 22:08:18 +0100

    0. Dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a model for a dog
+ * @name: the dog's mame
+ * @age: the age
+ * @owner: the owner
+ *
+ * Description: Longer description
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

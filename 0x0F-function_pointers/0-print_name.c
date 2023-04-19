d1af7653abad36b1f91029f62a8429eefd15a162
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-19 16:51:06 +0100

    0. Print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: the function to call
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name)
+		f(name);
+}

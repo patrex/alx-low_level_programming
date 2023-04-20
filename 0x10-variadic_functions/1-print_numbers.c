0b70b1168f1cffcc86262d0ba3554ef530054721
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-20 18:09:50 +0100

    1. Print numbers: fix -> refactor print last line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,6 +21,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (separator && i < (n - 1))
 			printf("%s", separator);
 	}
+	printf("\n");
 	va_end(list);
-	puts("\n");
 }

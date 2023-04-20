ea08eea53553cc70822d6ac4fd1e5af7909f7bde
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-04-20 17:54:50 +0100

    1. Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints all input nos followed by a new line
+ * @n: the count of numbers sent
+ * @separator: the char to use as a sep'tor
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int num;
+	unsigned int i = 0;
+	va_list list;
+
+	va_start(list, n);
+
+	for (; i < n;  i++)
+	{
+		num = va_arg(list, int);
+		printf("%d", num);
+		if ((n - i) == 1)
+			continue;
+		printf("%s", separator);
+	}
+	va_end(list);
+	puts("\n");
+}

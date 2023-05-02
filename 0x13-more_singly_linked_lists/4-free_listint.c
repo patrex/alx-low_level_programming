6f5e66d788ab761ac85f79d58b711d7baf475ee2
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-05-03 00:57:31 +0100

    Finish implementing mandatory tasks

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees the mem held by a linked list
+ * @head: head of a list.
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while ((temp = head) != NULL)
+	{
+		head = head->next;
+		free(tmp);
+	}
+}

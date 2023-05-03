5234d5859c4e310d98e502524430e01c492d463c
Patsoks Patsokari <patsoks.sokari@gmail.com>
2023-05-03 01:30:18 +0100

    inspect compile err on task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,15 +1,12 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node
- * at a given position.
+ * insert_nodeint_at_index - inserts a new node at some position.
  * @head: head of a list.
- * @idx: index of the list where the new node is
- * added.
+ * @idx: index of the list where the new node is to be inserted
  * @n: integer element.
  *
- * Return: the address of the new node, or NULL if it
- * failed.
+ * Return: the address of the new node, NULL on fail
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
